1fc15f569297744ccd36dfb6e1d80614fbf57409
evgeniyvojtik <jek_311@list.ru>
2021-03-05 23:39:24 +0300

    minor fixes commit

--- scheduler_APP/settings.py
+++ scheduler_APP/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +129,7 @@ CACHES = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'event_manager.SchedulerUser'
 
