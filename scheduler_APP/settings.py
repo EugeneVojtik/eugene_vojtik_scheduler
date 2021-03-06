c6b6a3e50e290f26e5b93d26934749ea6cf84990
evgeniyvojtik <jek_311@list.ru>
2021-03-06 16:04:08 +0300

    timezones fix

--- scheduler_APP/settings.py
+++ scheduler_APP/settings.py
@@ -156,7 +156,7 @@ REMIND_OPTIONS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'evtestingemail@gmail.com'
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = "testtest1234567"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
